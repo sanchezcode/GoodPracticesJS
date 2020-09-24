bfb26e878c0b76725cdba481c13fb043fc5339c1
sanchezcode <sanchezcode7@gmail.com>
2020-09-23 21:06:26 -0500

    sanchezcode

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Varaibles
+const btn = document.getElementById('btn');
+
+//Funciones
+function ejecutarAlerta() {
+    alert("Prueba de código");
+}
+
+//Onclicks
+btn.onclick = function () {
+    ejecutarAlerta();
+}
